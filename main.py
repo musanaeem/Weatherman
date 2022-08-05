c14467815d142e696713a8670f0bab4b14cd01ee
Musa Naeem <musa.naeem@arbisoft.com>
2022-08-05 14:56:03 +0500

    Multiple Reports Version

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from weather import Weatherman
 parser = argparse.ArgumentParser(description="A weather reporting application")
 parser.add_argument('path',
                     help="Path to file Extraction")
-parser.add_argument('-e', '--Temp_Humidity',
+parser.add_argument('-e', '--Temp_Humidity', nargs='+',
                     help="Report Highest and Lowest Temp and Humidity")
-parser.add_argument('-a', '--Avg_TH',
+parser.add_argument('-a', '--Avg_TH', nargs='+',
                     help="Report Average Temp and Humidity")
-parser.add_argument('-c', '--chart_TH',
+parser.add_argument('-c', '--chart_TH', nargs='+',
                     help="Report Highest and Lowest Temp and Humidity in chart form")
 parser.add_argument('-b', '--chart_TH_bonus', type=bool, default=False,  # Bonus task to print Temp together
                     help="Report Highest and Lowest Temp together chart form")
@@ -20,6 +20,7 @@ args = parser.parse_args()
 
 # main function to run the code
 if __name__ == '__main__':
+
     w = Weatherman(args.path, args.Temp_Humidity, args.Avg_TH, args.chart_TH, args.chart_TH_bonus)
     w.run()
 
--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 from os import path
-import zipfile
 import sys
 from termcolor import colored
+import zipfile
 
 
 class Weatherman:  # Weatherman Application in one class
@@ -12,6 +12,9 @@ class Weatherman:  # Weatherman Application in one class
     e_result = dict()
     a_result = dict()
     c_result = dict()
+
+    # Report Number to print
+    report_num = 1
 
     # Initialize Lists in these indexes of dictionary
     c_result["max_temp"] = []
@@ -32,73 +35,119 @@ class Weatherman:  # Weatherman Application in one class
         self.__c = c
         self.__b = b
 
+        self.__curr_e = None
+        self.__curr_a = None
+        self.__curr_c = None
+
     # Method to extract weather files to specified path
     def Extract(self):
         # Extract the weatherman.zip file to path
         with zipfile.ZipFile("weatherfiles.zip", 'r') as zip_ref:
             zip_ref.extractall(self.__path)
 
-    # Method to Print report of calculated results
-    def Print_report(self, mode):
-
-        # Checking which argument is true to print accordingly
+    def getData(self, mode):
         if mode == "e":
 
             high_date = Weatherman.e_result['maxdate']
             low_date = Weatherman.e_result['mindate']
+            high_temp = Weatherman.e_result['maxtemp']
+            low_temp = abs(Weatherman.e_result['mintemp'])
 
             high_date = high_date.split("-")  # Separating date into day, month and year
             low_date = low_date.split("-")
 
-            # Making use of format strings to output results
-            print(f"""
-                Highest: {Weatherman.e_result['maxtemp']}C on {Weatherman.months[int(high_date[1])-1]} {high_date[2]}
-                Lowest: {Weatherman.e_result['mintemp']}C on {Weatherman.months[int(low_date[1])-1]} {low_date[2]}
-                    """)
+            high_month = Weatherman.months[int(high_date[1])-1]
+            high_year = high_date[2]
+            low_month = Weatherman.months[int(low_date[1])-1]
+            low_year = low_date[2]
+
+            return high_temp, low_temp, high_month, low_month, high_year, low_year
 
         elif mode == "a":
+            high_avg_temp = round(Weatherman.a_result['avgmaxtemp'])
+            low_avg_temp = round(abs(Weatherman.a_result['avgmintemp']))
+            mean_avg_hum = round(Weatherman.a_result['avghumid'])
 
-            print(f"""
-                Highest Average: {round(Weatherman.a_result['avgmaxtemp'])}C
-                Lowest Average: {round(Weatherman.a_result['avgmintemp'])}C
-                Average Mean Humidity: {round(Weatherman.a_result['avghumid'])}%
-                    """)
+            return high_avg_temp, low_avg_temp, mean_avg_hum
 
         elif mode == "c":
-
+            max_pads = []
+            min_pads = []
             max_pad = ""
             min_pad = ""
 
+            max_temp = Weatherman.c_result["max_temp"]
+            min_temp = Weatherman.c_result["min_temp"]
+
             # Run loop for all the days logged
-            for i in range(len(Weatherman.c_result["max_temp"])):
-                if i > 30:  # Break if days somehow exceed 31 days
-                    break
+            for i in range(len(max_temp)):
 
                 # Making '+' Strings to pad to output
-                for j in range(Weatherman.c_result["max_temp"][i]):
+                for j in range(max_temp[i]):
                     max_pad += "+"
 
-                for j in range(Weatherman.c_result["min_temp"][i]):
+                for j in range(abs(min_temp[i])):
                     min_pad += "+"
 
                 # Adding color to strings
                 max_pad = colored(max_pad, 'red')
                 min_pad = colored(min_pad, 'blue')
 
+                max_pads.append(max_pad)
+                min_pads.append(min_pad)
+
+                max_pad = ""
+                min_pad = ""
+
+            return max_temp, min_temp, max_pads, min_pads
+
+    # Method to Print report of calculated results
+    def Print_report(self, mode):
+
+        # Checking which argument is true to print accordingly
+        if mode == "e":
+            high_temp, low_temp, high_month, low_month, high_year, low_year = self.getData("e")
+
+            # Making use of format strings to output results
+            print(
+            f"""Report Number {Weatherman.report_num}
+                Highest: {high_temp}C on {high_month} {high_year}
+                Lowest: {low_temp}C on {low_month} {low_year}
+                    """)
+            Weatherman.report_num += 1
+
+        elif mode == "a":
+            high_avg_temp, low_avg_temp, mean_avg_hum = self.getData("a")
+            print(
+            f"""Report Number {Weatherman.report_num}
+                Highest Average: {high_avg_temp}C
+                Lowest Average: {low_avg_temp}C
+                Average Mean Humidity: {mean_avg_hum}%
+                    """)
+            Weatherman.report_num += 1
+
+        elif mode == "c":
+
+            max_temp, min_temp, max_pads, min_pads = self.getData("c")
+
+            print(f'''Report Number {Weatherman.report_num}''')
+
+            # Run loop for all the days logged
+            for i in range(len(max_temp)):
+
                 # If !b, Print according to c argument, else print according to bonus task
                 if self.__b == 0:
-                    print(f"""
-                    {i+1} {max_pad} {Weatherman.c_result["max_temp"][i]}C
-                    {i+1} {min_pad} {Weatherman.c_result["min_temp"][i]}C""")
+                    print(
+                f"""        
+                {i+1} {max_pads[i]} {max_temp[i]}C
+                {i+1} {min_pads[i]} {min_temp[i]}C""")
 
-                    max_pad = ""
-                    min_pad = ""
                 else:
-                    print(f"""
-                    {i + 1} {min_pad}{max_pad} {Weatherman.c_result["min_temp"][i]}C - {Weatherman.c_result["max_temp"][i]}C""")
+                    print(
+                f"""
+                {i + 1} {min_pads[i]}{max_pads[i]} {min_temp[i]}C - {max_temp[i]}C""")
 
-                    max_pad = ""
-                    min_pad = ""
+            Weatherman.report_num += 1
 
     # Method to read lines from the file and return
     def Reader(self, mode):
@@ -111,30 +160,20 @@ class Weatherman:  # Weatherman Application in one class
         mult_lines = []
 
         if mode == "e":
-            if self.__e is None:
-                raise Exception("Enter an argument for -e")
-            else:
-                date = self.__e.split('/')
+            date = self.__curr_e.split('/')
 
         elif mode == "a":
-            if self.__a is None:
-                raise Exception("Enter an argument for -a")
-            else:
-                date = self.__a.split('/')
+            date = self.__curr_a.split('/')
 
         elif mode == "c":
-            if self.__c is None:
-                raise Exception("Enter an argument for -c")
-            else:
-                date = self.__c.split('/')
-
-            if len(date)<2:
+            date = self.__curr_c.split('/')
+            if len(date) < 2:
                 raise Exception("Please Enter the correct year and month for -c")
 
-        if date == "":
-            raise Exception("Date not Entered")
+        if len(date[0]) != 4:
+            raise Exception("Wrong date Format entered")
 
-        elif len(date) < 2:
+        if len(date) < 2:
             for month in Weatherman.months:
                 file = pth + date[0] + "_" + month + ".txt"
 
@@ -146,14 +185,19 @@ class Weatherman:  # Weatherman Application in one class
             if not exist:
                 raise Exception("Files for this year are not in the system.")
         else:
-            file = pth + date[0] + "_" + Weatherman.months[int(date[1])-1] + ".txt"
+            mon = int(date[1])
+            if 0 < mon < 13:
+                file = pth + date[0] + "_" + Weatherman.months[mon-1] + ".txt"
 
-            if path.exists(file):
-                f = open(file, 'r')
-                lines = f.readlines()
-                mult_lines.append(lines)
+                if path.exists(file):
+                    f = open(file, 'r')
+                    lines = f.readlines()
+                    mult_lines.append(lines)
+                else:
+                    raise Exception("No such file exists. Please enter file that is in the system")
+
             else:
-                raise Exception("No such file exists. Please enter file that is in the system")
+                raise Exception("Wrong format of month entered")
 
         return mult_lines
 
@@ -240,8 +284,14 @@ class Weatherman:  # Weatherman Application in one class
         self.Extract()
 
         if self.__e is not None:
-            self.Calculate_Results("e")
+            for e in self.__e:
+                self.__curr_e = e
+                self.Calculate_Results("e")
         if self.__a is not None:
-            self.Calculate_Results("a")
+            for a in self.__a:
+                self.__curr_a = a
+                self.Calculate_Results("a")
         if self.__c is not None:
-            self.Calculate_Results("c")
+            for c in self.__c:
+                self.__curr_c = c
+                self.Calculate_Results("c")
